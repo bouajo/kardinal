1242c803b3e9e7e487115935d92cd046db48f1c8
Jonathan Bouaziz <jonathan.bouaziz@gmail.com>
2025-03-31 10:12:40 +0200

    Initial commit

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,21 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('@expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
-const defaultConfig = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-module.exports = {
-  ...defaultConfig,
-  resolver: {
-    ...defaultConfig.resolver,
-    sourceExts: [...defaultConfig.resolver.sourceExts, 'jsx', 'js', 'ts', 'tsx'],
+// Add support for importing SVG files
+config.resolver.sourceExts.push('svg');
+config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+
+// Add support for Reanimated
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'jsx', 'js', 'ts', 'tsx', 'cjs', 'mjs', 'json'];
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true,
   },
-}; 
+});
+
+// Export the config
+module.exports = withNativeWind(config, { input: './global.css' }); 
